6f161b886d6fb092985f76a9c1b1589cba8b6198
Leo Ferreira <leonardoferreira@protonmail.com>
2022-12-08 22:16:02 -0300

    client room registration

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5293");
+
+var client = new Groom.GroomClient(channel);
+
+Console.Write("Enter room name to register:");
+var roomName = Console.ReadLine();
+
+var resp = client.RoomRegistration(new RoomRegistrationRequest { RoomName = roomName });
+Console.WriteLine($"Room Id: {resp.RoomId}");
+
+Console.Read();
